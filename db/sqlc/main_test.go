10c0ee272b5e0f20d1c30844e33e93a7b590fd64
kukim <kukim.kim@groundx.xyz>
2023-03-13 20:26:51 +0900

    add store 코드 구현과 테스트(트랜잭션 X)

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -9,26 +9,29 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var testQueries *Queries
-
 const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
+var testQueries *Queries
+var testDB *sql.DB
+
 /*
 https://medium.com/goingogo/why-use-testmain-for-testing-in-go-dafb52b406bc
-m *testing.M는 beforeAll, afterAll이라고 생각하면 좋음 (m.Run 기준
+m *testing.M는 beforeAll, afterAll 이라고 생각하면 좋음 (m.Run 기준
 */
 func TestMain(m *testing.M) {
 	log.Println("--- Before Test ---")
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
-		log.Fatalln("cannot connect to db: ", err)
+		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 	code := m.Run()
 	log.Println("--- AfterAll Test ---")
 	os.Exit(code)
+
 }
